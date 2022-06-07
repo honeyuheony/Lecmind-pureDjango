145b425639dcc5057931df79e22574ab1ca78f8d
Kuhy <kwh1019_@naver.com>
2022-06-07 18:01:23 +0900

    우헌 : 모델 오류 수정

--- patternProject/analysis/models.py
+++ patternProject/analysis/models.py
@@ -18,13 +18,23 @@ class Analysis(models.Model):
     anlysis_idx = models.AutoField(primary_key=True)
     lecture = models.ForeignKey(
         Lecture, on_delete=models.CASCADE, related_name='analysis_lecture')
+    total_frames = models.IntegerField(null=True, blank=True)
+    focus_frames = models.IntegerField(null=True, blank=True)
+    concentration_rate = models.FloatField(null=True, blank=True)
+
+    def __str__(self):
+        return self.anlysis_idx
     
+
+
+class Interaction(models.Model):
     EVENT = (
         ('pause','pause'),
         ('redo','redo'),
         ('fast_forward','fast_forward'),
         ('rewind','rewind'),
     )
+    interaction_idx = models.AutoField(primary_key=True)
     lecture = models.ForeignKey(
         Lecture, on_delete=models.CASCADE, related_name='interaction_lecture')
     interaction_type = models.CharField(choices=EVENT, max_length=20)
@@ -34,12 +44,3 @@ class Analysis(models.Model):
     def __str__(self):
         return str(self.interaction_idx)
 
-class Review_section(models.Model):
-    review_section_idx = models.AutoField(primary_key=True)
-    lecture = models.ForeignKey(
-        Lecture, on_delete=models.CASCADE, related_name='reviewsection_lecture')
-    section_start = models.CharField(max_length=20)
-    section_end = models.CharField(max_length=20)
-    
-    def __str__(self):
-        return str(self.review_section_idx)
